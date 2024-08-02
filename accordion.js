1d35695c7847caad52296c73058bd220c0223ebc
Hileene <92981899+Hileene@users.noreply.github.com>
2024-08-02 16:00:41 +0200

    Updated the logic to toggle icon plus button

--- accordion.js
+++ accordion.js
@@ -8,9 +8,13 @@ questionBlocks.forEach((questionBlock) => {
     answerBlock.classList.toggle('answer-visible')
 
     // Toggle the icon based on answer visibility
-    const icon = questionBlock.querySelector('#icon-collapse')
-    icon.src = answerBlock.classList.contains('answer-visible')
-      ? 'assets/images/icon-minus.svg'
-      : 'assets/images/icon-plus.svg'
+    const icon = questionBlock.querySelectorAll(
+      '#icon-collapse-1, #icon-collapse-2, #icon-collapse-3, #icon-collapse-4'
+    )
+    icon.forEach((icon) => {
+      icon.src = answerBlock.classList.contains('answer-visible')
+        ? 'assets/images/icon-minus.svg'
+        : 'assets/images/icon-plus.svg'
+    })
   })
 })
